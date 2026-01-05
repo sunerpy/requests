d855fae61b6482126fb0acd2c769a4711c044ff8
sunerpy <nkuzhangshn@gmail.com>
2026-01-05 12:37:06 +0800

    style(lint): Remove unused header constants

--- methods.go
+++ methods.go
@@ -10,15 +10,9 @@ import (
 	"github.com/sunerpy/requests/url"
 )
 
-// Common header constants to avoid repeated string allocations
 const (
 	contentKey      = "Content-Type"
 	jsonContentType = "application/json"
-	// Additional header constants for internal use
-	headerAccept        = "Accept"
-	headerAuthorization = "Authorization"
-	headerUserAgent     = "User-Agent"
-	headerContentLength = "Content-Length"
 )
 
 // ============================================================================
